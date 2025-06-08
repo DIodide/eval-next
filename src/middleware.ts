acb095d1e04d82346e0cb48d0717989196f5f190
DIodide <ibraheem.amin3@gmail.com>
2025-06-07 20:57:11 -0400

    add comment for middleware debug mode for future ref

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ const isProtectedRoute = createRouteMatcher(['/dashboard(.*)'])
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
-})
+}) // set debug to true to see the middleware in action
 
 
 
