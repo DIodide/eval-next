8232d0396d04c43979b7f4b1ec1709298a442e80
DIodide <ibraheem.amin3@gmail.com>
2025-06-07 21:38:46 -0400

    fix trusted by image links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -128,9 +128,9 @@ export default function HomePage() {
         <div className="container mx-auto px-6">
           <h2 className="font-orbitron text-2xl text-center text-white mb-8 tracking-wide">TRUSTED BY</h2>
           <div className="flex flex-wrap justify-center items-center gap-8 opacity-60">
-            <Image src="/partners/gse/gse_logo.png" alt="Garden State Esports" width={150} height={60} />
-            <Image src="/partners/psv/psv_logo.png" alt="Princeton Student Ventures" width={150} height={60} />
-            <Image src="/partners/keller/keller_logo.png" alt="Keller Center" width={150} height={60} />
+            <Image src="/partners/gse/GSE_LOGO.png" alt="Garden State Esports" width={150} height={60} />
+            <Image src="/partners/psv/psv.png" alt="Princeton Student Ventures" width={150} height={60} />
+            <Image src="/partners/keller/keller.png" alt="Keller Center" width={150} height={60} />
           </div>
         </div>
       </section>
