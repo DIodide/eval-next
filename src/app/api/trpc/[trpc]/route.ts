72d87ecb281e51f67a98efa1b64c8e51a86f755a
DIodide <ibraheem.amin3@gmail.com>
2025-06-10 15:44:04 -0400

    Add clerk context (revisit if trpc doesn't work later)

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -5,13 +5,18 @@ import { env } from "@/env";
 import { appRouter } from "@/server/api/root";
 import { createTRPCContext } from "@/server/api/trpc";
 
+// CLERK
+import { createClerkContext } from '@/server/context'
+
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
  */
 const createContext = async (req: NextRequest) => {
+  const clerkContext = await createClerkContext()
   return createTRPCContext({
     headers: req.headers,
+    ...clerkContext,
   });
 };
 
---
+++ src/server/context.ts
@@ -0,0 +1,10 @@
+// Following https://clerk.com/docs/references/nextjs/trpc
+
+import { auth } from '@clerk/nextjs/server'
+
+export const createClerkContext = async () => {
+  return { auth: await auth() }
+}
+
+// Create the tRPC context
+export type Context = Awaited<ReturnType<typeof createClerkContext>>
