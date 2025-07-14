6abd95f879523c70ede7a7c157b3661ec6439d3d
DIodide <ibraheem.amin3@gmail.com>
2025-07-14 10:12:40 -0400

    always create person profiles

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -13,7 +13,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: env.NEXT_PUBLIC_POSTHOG_HOST ?? 'https://us.i.posthog.com',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      person_profiles: 'always', // or 'always' to create profiles for anonymous users as well
       defaults: '2025-05-24'
     })
   }, [])
